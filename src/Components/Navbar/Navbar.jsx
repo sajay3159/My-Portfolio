389f8d2c7be1d8cb57540f986098c2adc5943a5f
Ajay Sahani <sajay3159@gmail.com>
2025-03-15 10:41:35 +0530

    fix the Navbar issue

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,24 +13,58 @@ const Navbar = () => {
     const openMenu = () => {
         menuRef.current.style.right = "0";
     }
+
     const closeMenu = () => {
         menuRef.current.style.right = "-350px";
     }
+
+    const handleMenuClick = (menuName) => {
+        setMenu(menuName);
+        closeMenu();
+    }
+
     return (
         <div className='navbar'>
             <img src={logo} alt="" />
             <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
             <ul ref={menuRef} className="nav-menu">
                 <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
-                <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#mywork'><p onClick={() => setMenu("portfolio")}>Portfolio</p></AnchorLink>{menu === "portfolio" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
+                <li>
+                    <AnchorLink className='anchor-link' href='#home'>
+                        <p onClick={() => handleMenuClick("home")}>Home</p>
+                    </AnchorLink>
+                    {menu === "home" ? <img src={underline} alt='' /> : <></>}
+                </li>
+                <li>
+                    <AnchorLink className='anchor-link' offset={50} href='#about'>
+                        <p onClick={() => handleMenuClick("about")}>About Me</p>
+                    </AnchorLink>
+                    {menu === "about" ? <img src={underline} alt='' /> : <></>}
+                </li>
+                <li>
+                    <AnchorLink className='anchor-link' offset={50} href='#services'>
+                        <p onClick={() => handleMenuClick("services")}>Services</p>
+                    </AnchorLink>
+                    {menu === "services" ? <img src={underline} alt='' /> : <></>}
+                </li>
+                <li>
+                    <AnchorLink className='anchor-link' offset={50} href='#mywork'>
+                        <p onClick={() => handleMenuClick("portfolio")}>Portfolio</p>
+                    </AnchorLink>
+                    {menu === "portfolio" ? <img src={underline} alt='' /> : <></>}
+                </li>
+                <li>
+                    <AnchorLink className='anchor-link' offset={50} href='#contact'>
+                        <p onClick={() => handleMenuClick("contact")}>Contact</p>
+                    </AnchorLink>
+                    {menu === "contact" ? <img src={underline} alt='' /> : <></>}
+                </li>
             </ul>
-            <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
+            <div className="nav-connect">
+                <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
