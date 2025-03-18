fb8d0c80343f919edf0040c2f17a304f34d5d3bb
Ajay Sahani <sajay3159@gmail.com>
2025-03-18 20:00:42 +0530

    change the image of about profile img

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
-import profile_img from '../../assets/about_profile.jpeg'
+import profile_img from '../../assets/about_profile.jpg'
 
 const About = () => {
     return (
