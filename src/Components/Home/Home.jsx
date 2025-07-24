813d2a101c18f410e2ae14651f2a726f5b27677b
Ajay Sahani <115401286+sajay3159@users.noreply.github.com>
2025-07-24 22:33:54 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     return (
         <div id="home" className='home'>
             <img src={profile_img} alt="Profile" />
-            <h1><span>I'm Ajay Sahani</span>, Frontend Developer </h1>
-            <p>I am a Frontend Developer from Vadodara, Gujarat with 2 years in a Company, Emarket Experts</p>
+            <h1><span>I'm Ajay Sahani</span>, Software Developer </h1>
+            <p>I am a Software Developer from Vadodara, Gujarat with 2 years in a Company, Emarket Experts</p>
             <div className="home-action">
                 <div className="home-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
                 <a href="/Ajay_resume.pdf" download className="home-resume">My Resume</a>
