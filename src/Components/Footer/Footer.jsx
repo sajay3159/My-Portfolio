961fb3bfa756e0d0f49ca4db4c9a75e51e21d4f7
Ajay Sahani <115401286+sajay3159@users.noreply.github.com>
2025-07-24 22:40:22 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="footer-top">
                 <div className="footer-top-left">
                     <img src={footer_logo} alt="" />
-                    <p>I am a Frontend Developer from Vadodara, Gujarat with 2 years in a Company, Emarket Experts</p> 
+                    <p>I am a Software Developer from Vadodara, Gujarat with 2 years in a Company, Emarket Experts</p> 
                 </div>
                 <div className="footer-top-right">
                     <div className="footer-email-input">
